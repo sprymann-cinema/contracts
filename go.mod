c8270c20c2bd7df98ee9e1fecd9bda57ad927bf8
George <lyakhovgm@gmail.com>
2026-09-16 16:48:29 +0300

    feat: add media.proto

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sprymann-cinema/contracts
+
+go 1.25.0
---
+++ scripts/generate_go.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+echo "Generating Go protobuf (media)..."
+
+protoc -I ./proto \
+    --go_out=./gen/go \
+    --go-grpc_out=./gen/go \
+    --go_opt=module=github.com/sprymann-cinema/contracts \
+    --go-grpc_opt=module=github.com/sprymann-cinema/contracts \
+    ./proto/media.proto
+
+echo "Go generation complete."
